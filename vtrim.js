8584256c8df13d1c11cf6be890c60a70f428f826
Alvaro Calace <alvarocalace@hotmail.com>
2017-06-22 15:34:46 -0300

    Use AB-loops to define interval.

--- vtrim.js
+++ vtrim.js
@@ -20,7 +20,6 @@
 // State
 
 var state = (function state_initializer() {
-	var start = null;
 	var ffmpeg = get_opt('ffmpeg', 'ffmpeg');
 	var bps = get_opt('bps', false);
 	var size_hint = parse_size_hint(get_opt('size-hint', false));
@@ -28,8 +27,6 @@ var state = (function state_initializer() {
 	var detached = get_opt('detached', false) === 'true';
 	
 	return {
-		get_start: function () { return start; },
-		set_start: function (value) { start = value; },
 		get_ffmpeg: function () { return ffmpeg; },
 		get_bps: function () { return bps; },
 		get_size_hint: function () { return size_hint; },
@@ -47,6 +44,10 @@ function is_string(value) {
 
 function is_object(value) {
 	return typeof value === 'object';
+}
+
+function is_number(value) {
+	return typeof value === 'number';
 }
 
 
@@ -95,8 +96,18 @@ function calc_size(hint, video_size) {
 
 // mp.* wrappers
 
-function get_playback_time() {
-	return mp.get_property_number('playback-time');
+function get_ab_loop() {
+	var a = mp.get_property_number('ab-loop-a');
+	var b = mp.get_property_number('ab-loop-b');
+
+	return {
+		a: a,
+		b: b
+	};
+}
+
+function cmd_ab_loop() {
+	mp.command('ab-loop');
 }
 
 function get_path() {
@@ -131,39 +142,11 @@ function get_video_size() {
 }
 
 
-// Formatters
+// ffmpeg
 
 function format_output_file(name, ext, start, end) {
-	return [name, format_time(start, true), format_time(end, true), ext].join('.');
+	return name + ' [' + start.toFixed(3) + '-' + end.toFixed(3) + '].' + ext;
 }
-
-function format_unit(value) {
-	value = Math.floor(value).toString();
-	return value.length === 1 ? '0' + value : value;
-}
-
-function format_ms(ms) {
-	return ms.toFixed(3).substring(2);
-}
-
-function format_time(time, for_output) {
-	var sep = ':';
-	var ms_sep = '.';
-	var seconds = Math.floor(time);
-	var ms = time - seconds;
-	var hh = seconds / 3600;
-	var mm = seconds % 3600 / 60;
-	var ss = seconds % 60;
-
-	if (for_output) {
-		sep = ms_sep = '-';
-	}
-	
-	return format_unit(hh) + sep + format_unit(mm) + sep + format_unit(ss) + ms_sep + format_ms(ms);
-}
-
-
-// ffmpeg
 
 function calc_size_ffmpeg(hint, video_size) {
 	var result = calc_size(hint, video_size);
@@ -179,7 +162,6 @@ function get_ffmpeg_args(start, end, mode) {
 	var bps = state.get_bps();
 	var size_hint = state.get_size_hint();
 	var loglevel = 'error';
-
 	var args = [
 		state.get_ffmpeg(),
 		'-n',
@@ -239,36 +221,28 @@ function run_ffmpeg(start, end, mode) {
 // Handler helpers
 
 function handle_start(no_subs, no_audio) {
-	var start = state.get_start();
-	var playback_time = get_playback_time();
+	var ab_loop = get_ab_loop();
 	var mode = {
 		no_subs: no_subs,
 		no_audio: no_audio
 	};
 	
-	if (start === null) {
-		osd_message(format_time(playback_time));
-		state.set_start(playback_time);
+	if (is_number(ab_loop.a) && is_number(ab_loop.b)) {
+		trim_video(ab_loop.a, ab_loop.b, mode);
 	} else {
-		trim_video(start, playback_time, mode);
+		osd_message('AB-loop not defined.');
 	}
 }
 
-function trim_video(start, end, mode) {
-	var formatted_times = format_time(start) + ' / ' + format_time(end);
-	
+function trim_video(start, end, mode) {	
 	if (end > start) {
-		osd_message(formatted_times +  ' '  + mode_info(mode));
+		osd_message(mode_info(mode));
 		var result = run_ffmpeg(start, end, mode);
 		osd_message(result.info);
-		reset();
+		cmd_ab_loop();
 	} else {
-		osd_message('End time can\'t be higher than start time (' + formatted_times + ').');
+		osd_message('End time can\'t be higher than start time.');
 	}
-}
-
-function reset() {
-	state.set_start(null);
 }
 
 function mode_info(mode) {
@@ -300,11 +274,6 @@ function handle_start_no_subs_no_audio() {
 	handle_start(true, true);
 }
 
-function handle_reset() {
-	osd_message('Reset.');
-	reset();
-}
-
 
 // Main
 
@@ -312,6 +281,5 @@ function handle_reset() {
 	mp.add_key_binding('n', handle_start_all_enabled);
 	mp.add_key_binding('shift+n', handle_start_no_subs);
 	mp.add_key_binding('ctrl+n', handle_start_no_audio);
-	mp.add_key_binding('ctrl+shift+n', handle_start_no_subs_no_audio);
-	mp.add_key_binding('alt+n', handle_reset);
+	mp.add_key_binding('alt+n', handle_start_no_subs_no_audio);
 }());
